a2093f690b7794f72d9697508c6712f5b90918fc
Rodrigo Vargas <rodrivh@gmail.com>
2023-05-06 14:14:05 -0700

    Added function to sort FBS games

---
+++ DataFunctions.py
@@ -0,0 +1,12 @@
+def get_fbs_games(api_instance,year):
+    gameslist = api_instance.get_games(year=year,division='fbs')
+    i = 0
+    while i < len(gameslist):
+        if(gameslist[i].home_division==gameslist[i].away_division=="fbs" 
+           and gameslist[i].completed==True
+           and gameslist[i].neutral_site==False):
+            i+=1
+            continue
+        else:
+            gameslist.remove(gameslist[i])
+    return gameslist
