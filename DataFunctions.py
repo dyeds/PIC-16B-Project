1ce5d747d8f1752b0919629cc32417300a2ddf69
Rodrigo Vargas <rodrivh@gmail.com>
2023-05-06 19:22:39 -0700

    Notebook works as normal bc API was restored
    Function splits fbs only games. Currently working in Games DF function on both notebook and .py files

--- DataFunctions.py
+++ DataFunctions.py
@@ -1,16 +1,10 @@
 def get_fbs_games(api_instance,year):
-    gameslist = api_instance.get_games(year=year,division='fbs')
+    gamelist = api_instance.get_games(year=year,division='fbs')
     
-    gameslist = [game for game in gameslist if (gameslist[i].home_division==gameslist[i].away_division=="fbs" 
-                                                and gameslist[i].completed==True)]
-    
-    # i = 0
-    # while i < len(gameslist):
-    #     if(gameslist[i].home_division==gameslist[i].away_division=="fbs" 
-    #        and gameslist[i].completed==True
-    #        and gameslist[i].neutral_site==False):
-    #         i+=1
-    #         continue
-    #     else:
-    #         gameslist.remove(gameslist[i])
-    return gameslist
+    gamelist = [game for game in gamelist if (game.home_division==game.away_division=="fbs" 
+                                                and game.completed==True)]
+    return gamelist
+
+def df_from_games(gamelist):
+    gamestr = str(gamelist).split('\n')
+    pass
