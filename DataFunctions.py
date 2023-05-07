d10791f800c6477271aa7b5ffd88606d74678804
Rodrigo Vargas <rodrivh@gmail.com>
2023-05-06 20:44:16 -0700

    Created SQL database and added Games Dataframe

--- DataFunctions.py
+++ DataFunctions.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def get_fbs_games(api_instance,year):
     gamelist = api_instance.get_games(year=year,division='fbs')
     
@@ -6,5 +8,14 @@ def get_fbs_games(api_instance,year):
     return gamelist
 
 def df_from_games(gamelist):
-    gamestr = str(gamelist).split('\n')
-    pass
+    gamelist = [str(game).split("\n") for game in gamelist]
+    gamelist = [[s[2:-1] for s in game] for game in gamelist]
+    gamelist = [[s.replace("'","").split(": ") for s in game] for game in gamelist]
+    gamelist = [{s[0]:s[1] for s in game} for game in gamelist]
+    gamemetrics = ['away_conference','away_id','away_points','away_team',
+                   'home_conference','home_id','home_points','home_team',
+                   'id', 'season','neutral_site']
+    gamelist = [{x:game[x] for x in game if x in gamemetrics} for game in gamelist]
+    
+    df = pd.DataFrame(gamelist)
+    return df
